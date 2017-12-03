a63dce59585c63991a4c9b0ae9353d9df5983dce
Tom Russell <tomalrussell@gmail.com>
2017-12-03 15:50:09 +0000

    Add time series and sidebar for tabs

--- static/js/main.js
+++ static/js/main.js
@@ -13,14 +13,105 @@
         }
     }
 
-    function setup(){
-        var map = L.map('map').setView([51.505, -0.09], 11);
+    function chart(){
+        Plotly.d3.csv("../data/lower_thames_river_flows.csv", function(err, rows){
+            var trace = {
+                type: "scatter",
+                mode: "lines",
+                name: 'Lower Thames River Flows',
+                x: rows.map(function(row) { return row['date']; }),
+                y: rows.map(function(row) { return row['flow']; }),
+                line: {color: '#17BECF'}
+            }
+
+            var data = [trace];
+            var start_end = ['2070-01-01', '2099-12-30']
+
+            var layout = {
+                title: 'Lower Thames River Flows',
+                xaxis: {
+                    autorange: true,
+                    range: start_end,
+                    rangeselector: {buttons: [
+                        {
+                            count: 1,
+                            label: '1y',
+                            step: 'year',
+                            stepmode: 'backward'
+                        },
+                        {
+                            count: 10,
+                            label: '10y',
+                            step: 'year',
+                            stepmode: 'backward'
+                        },
+                        {step: 'all'}
+                    ]},
+                    rangeslider: {range: start_end},
+                    type: 'date'
+              },
+              yaxis: {
+                    autorange: true,
+                    range: [0, 45000],
+                    type: 'linear'
+              }
+            };
+
+            Plotly.newPlot('chart', data, layout);
+        });
+    }
+
+    function map(){
+        var positron = L.tileLayer(basemaps.carto_positron.url,
+            {id: 'Map', attribution: basemaps.carto_positron.attribution}
+        );
+        var satellite = L.tileLayer(basemaps.esri_worldimagery.url,
+            {id: 'Satellite', attribution: basemaps.esri_worldimagery.attribution}
+        );
+
+        var map = L.map('map', {
+            center: [51.505, -0.09],
+            zoom: 11,
+            layers: [satellite, positron]
+        });
+
+        var baseMaps = {
+            "Satellite": satellite,
+            "Map": positron
+        };
+        L.control.layers(baseMaps).addTo(map);
 
         map.zoomControl.setPosition('bottomright');
+    }
 
-        L.tileLayer(basemaps.carto_positron.url, {
-            attribution: basemaps.carto_positron.attribution
-        }).addTo(map);
+    function pullout(){
+        var tabs = document.querySelectorAll('.pullout .tab');
+        for (let i = 0; i < tabs.length; i++) {
+            const tab = tabs[i];
+            tab.addEventListener("click", togglePullout);
+        }
+    }
+
+    function togglePullout(e){
+        var pullout = document.querySelector('.pullout');
+        if (pullout.classList.contains('active')){
+            pullout.classList.remove('active');
+        } else {
+            pullout.classList.add('active');
+        }
+    }
+
+    function setup(){
+        if (document.querySelector('#map')){
+            map();
+        }
+        if (document.querySelector('#chart')){
+            chart();
+        }
+        if (document.querySelector('.pullout')){
+            pullout();
+        }
+
     }
 
     setup();
